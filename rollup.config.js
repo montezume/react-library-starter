f4ba8c25300b7fd2eca6ddde88ce73383483f040
Malcolm <malcolmclaing@gmail.com>
2019-01-13 15:37:32 +0100

    feat: add umd bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,32 @@
 import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
 import cleanup from 'rollup-plugin-cleanup';
 import replace from 'rollup-plugin-replace';
+import camelcase from 'lodash.camelcase';
 import pkg from './package.json';
 
-const commonjs = require('rollup-plugin-commonjs');
-const nodeResolve = require('rollup-plugin-node-resolve');
+const capitalize = s => s[0].toUpperCase() + s.slice(1);
 
 const format = process.env.BUILD_FORMAT;
+
+const defaultGlobals = Object.keys(pkg.peerDependencies || {}).reduce(
+  (deps, dep) => {
+    // eslint-disable-next-line no-param-reassign
+    deps[dep] = capitalize(camelcase(dep));
+    return deps;
+  },
+  {}
+);
 
 // const esm = format === 'esm';
 const umd = format === 'umd';
 
 const config = {
   input: 'src/index.js',
+  output: {
+    globals: defaultGlobals,
+  },
   external: umd ? [] : Object.keys(pkg.peerDependencies) || [],
   plugins: [
     replace({
@@ -23,9 +37,6 @@ const config = {
     }),
     commonjs(),
     nodeResolve(),
-
-    // To remove comments, trim trailing spaces, compact empty lines,
-    // and normalize line endings
     cleanup(),
   ],
 };
